ff295d2bf6509d937492062750aefba4e33c62ec
Austregiselo Junior <austregiselosvj@gmail.com>
2024-01-27 16:18:35 -0300

    Criando as camadas de repositorio e servico para campeonato.

--- BackEnd_Java/api/src/main/java/br/edu/unipe/pos/api/model/Campeonato.java
+++ BackEnd_Java/api/src/main/java/br/edu/unipe/pos/api/model/Campeonato.java
@@ -1,14 +1,15 @@
 package br.edu.unipe.pos.api.model;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import jakarta.validation.constraints.NotEmpty;
-import org.springframework.boot.autoconfigure.web.WebProperties;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.io.Serializable;
 
+@Getter
+@Setter
+@Entity
 public class Campeonato implements Serializable {
 
     @Id
