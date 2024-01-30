04a208846811028b2a26142bf5cfa6915ece26f7
Austregíselo Soares de Vasconcelos Junior <105051189+Austregiselo-Junior@users.noreply.github.com>
2024-01-29 21:35:57 -0300

    Add files via upload
    Foi adicionado o controlle para Campeonato

--- BackEnd_Java/api/src/main/java/br/edu/unipe/pos/api/Controller/CampeonatoController.java
+++ BackEnd_Java/api/src/main/java/br/edu/unipe/pos/api/Controller/CampeonatoController.java
@@ -1,4 +1,54 @@
 package br.edu.unipe.pos.api.Controller;
 
+import br.edu.unipe.pos.api.Service.CampeonatoService;
+import br.edu.unipe.pos.api.model.Campeonato;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Collections;
+import java.util.List;
+
+@RestController
+@RequestMapping("/campeonato")
 public class CampeonatoController {
+
+    @Autowired
+    private CampeonatoService service;
+
+    @PostMapping
+    public ResponseEntity<Campeonato> cadastrar(@RequestBody Campeonato campeonato){
+        campeonato = service.cadastrar(campeonato);
+        return ResponseEntity.ok(campeonato);
+    }
+
+    @PutMapping
+    public ResponseEntity<Campeonato> alterar(@RequestBody Campeonato campeonato){
+        campeonato = service.cadastrar(campeonato);
+        return ResponseEntity.ok(campeonato);
+    }
+
+    @DeleteMapping
+    public ResponseEntity<Void> deletar(@PathVariable Integer id){
+        service.deletar(id);
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping
+    public ResponseEntity<Void> listar(){
+        service.listar();
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/nome/{nome}")
+    public ResponseEntity<List<Campeonato>> listarPorNome(@PathVariable String nome){
+        service.listarPorNome(nome);
+       return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/id")
+    public ResponseEntity<List<Campeonato>> consultarPorId(@PathVariable Integer id){
+        return ResponseEntity.ok(Collections.singletonList(service.copnsultarPorId(id)));
+    }
 }
