c1b7eae1c0cf87b6d23d5d102c07ef1a3ad8661c
Austregiselo Junior <austregiselosvj@gmail.com>
2024-07-01 11:48:46 -0300

    Subindo teste de Api com Cypress

--- Teste_Qualidade_de_Sofitware/cypress/e2e/backend.spec.cy.js
+++ Teste_Qualidade_de_Sofitware/cypress/e2e/backend.spec.cy.js
@@ -1,11 +1,33 @@
 /// <reference types="cypress"/>
 
 describe("Teste do back", () => {
-      it("Requisição Get", () => {
-        cy.request("https://httpbin.org/get").should(function(response){
+      it("Requisição Get com CEP válido", () => {
+        cy.request("viacep.com.br/ws/01001000/json").should(function(response){
         const {status, statusText} = response;
         expect(status).to.be.equal(200)
         expect(statusText).to.be.equal("OK");
+      })
+    })
+
+     it("Requisição Get com CEP inválido", () => {
+        cy.request({
+            url:"viacep.com.br/ws/010010003/json",
+            failOnStatusCode: false
+        }).should(function(response){
+        const {status, statusText} = response;
+        expect(status).to.be.equal(400)
+        expect(statusText).to.be.equal("Bad Request");
+      })
+    })
+
+       it("Requisição Get com CEP inválido com ultimo igual a T", () => {
+        cy.request({
+            url:"viacep.com.br/ws/0100100T/json",
+            failOnStatusCode: false
+        }).should(function(response){
+        const {status, statusText} = response;
+        expect(status).to.be.equal(400)
+        expect(statusText).to.be.equal("Bad Request");
       })
     })
 
